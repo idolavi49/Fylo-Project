a2c9d7fa9c5ba97dcdfb94a4f5556f97153aa957
idolavi49 <72653265+idolavi49@users.noreply.github.com>
2021-01-21 19:51:10 +0200

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,78 +1,82 @@
-var usedSize = 0;
-var leftSize = 50;
-var filesPropertyArray = [];
+var validFileExtensions = ["jpg", "jpeg", "png"];
+var usedStorage = 0;
+var myStorage = 50;
+var filesArray = [];
+var sizeFileLimit = 1.5;
+
+function convertBtyesToMB(size) {
+    var MB = Math.pow(1024, 2);
+
+    return size / MB;
+}
+
+function showErrorAlert(message) {
+    alert('There\'s a problem \n ' + message);
+    throw new Error('Error: ' + message);
+}
+
+
+function isFileValid(validTypesArr, type) {
+    return !!validTypesArr.find(function (t) {
+        return ("image/" + t) === type
+    });
+}
+
+function loadDataToPage() {
+    var precentegeStorage = ((usedStorage / myStorage) * 100).toFixed() + '%';
+
+    document.getElementById("usedSize").innerHTML = usedStorage.toFixed() + " MB";
+    document.getElementById("leftSize").innerHTML = (myStorage - usedStorage).toFixed();
+    document.getElementById("progressBarUsed").style.width = precentegeStorage;
+    document.getElementById("usedSizePercentage").innerHTML = precentegeStorage;
+};
+
+function createTableRow() {
+    nextRow = filesArray.length;
+    var table = document.getElementById("tableID");
+    var row = table.insertRow(nextRow);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = filesArray[nextRow - 1].fileName;
+    cell2.innerHTML = filesArray[nextRow - 1].fileSize + " KB";
+};
+
+function onUploadFile(ev) {
+    var message;
+    var file = ev.target.files[0];
+    var type = file.type;
+    var fileSize = convertBtyesToMB(file.size);
+    var name = file.name;
+
+    if (isFileValid(validFileExtensions, type)) {
+        if ((usedStorage + fileSize) > myStorage) {
+            message = 'There is not enough space free for this file';
+        } else if (fileSize > sizeFileLimit) {
+            message = 'File size is bigger from ' + sizeFileLimit + ' MB';
+        } else {
+            usedStorage += fileSize;
+            loadDataToPage();
+
+            //add the file name and size to the array for the table
+            var fileObject = {
+                fileName: name,
+                fileSize: (fileSize * 1024).toFixed(2)
+            };
+            filesArray.push(fileObject);
+            createTableRow();
+        }
+    } else {
+        message = 'Invalid File';
+    }
+
+
+    if (message) {
+        showErrorAlert(message);
+    }
+
+    ev.target.value = '';
+}
 
 window.onload = function () {
-    const realButton = document.getElementById("real-uploadfile-button");
-    const fakeButton = document.getElementById("fake-uploadfile-button");
-    var validFileExtensions = ["image/jpg", "image/jpeg", "image/png"];
-    loadDataToPage();
-
-    fakeButton.addEventListener("click", function () {
-        realButton.click();
-    });
-
-    realButton.addEventListener("change", function () {
-        let file = realButton.files;
-        let size = file[0].size;
-        let MBsize = size / (1000 * 1024);
-        let type = file[0].type;
-        let name = file[0].name;
-
-
-        //initialize first data to the page
-        loadDataToPage();
-
-
-        //vailidate file size
-        if (file.length > 0) {
-            if (size > 1500 * 1024) {
-                alert("File size is bigger from 1.5MB");
-                return;
-            }
-        }
-
-        //validate extentions
-        if (validFileExtensions.indexOf(type) === -1) {
-            alert("File is not an image (jpg/jpeg/png)");
-            return;
-        }
-
-        //validate left space
-        if (leftSize - MBsize < 0) {
-            alert("There is not enough space free for this file");
-            return;
-        }
-
-        //update the data after uploading the file and reload the page
-        usedSize = usedSize + MBsize;
-        leftSize = leftSize - MBsize;
-        loadDataToPage();
-
-        //add the file name and size to the array for the table
-        var fileObject = {
-            fileName: name,
-            fileSize: (size/1024).toFixed()
-        };
-        filesPropertyArray.push(fileObject);
-        setTable();
-    });
-
-    function loadDataToPage() {
-        document.getElementById("usedSize").innerHTML = usedSize.toFixed() + " MB";
-        document.getElementById("leftSize").innerHTML = leftSize.toFixed();
-        document.getElementById("progress-bar__usedID").style.width = ((usedSize / 50) * 100).toFixed() + '%';
-        document.getElementById("usedSize__percentage").innerHTML = ((usedSize / 50) * 100).toFixed() + '%';
-    };
-
-    function setTable() {
-        index = filesPropertyArray.length;
-        var table = document.getElementById("tableID");
-        console.log(table);
-        var row = table.insertRow(index);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        cell1.innerHTML = filesPropertyArray[index-1].fileName;
-        cell2.innerHTML = filesPropertyArray[index-1].fileSize + " KB";
-    };
+    document.getElementById("realUploadFileButton").addEventListener("change", onUploadFile);
 };
